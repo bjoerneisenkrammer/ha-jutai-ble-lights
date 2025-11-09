78b7ddd08c745227ec90c050df8b7c70f3e90776
Björn Eisenkrammer <1223223+bjoerneisenkrammer@users.noreply.github.com>
2025-11-09 18:19:11 +0100

    Refactor Dockerfile to use a virtual environment for Python dependencies and update config.yaml version to 0.1.0. Enhance run.sh to set DBUS address and activate the virtual environment.

--- jutai_ble_bridge/run.sh
+++ jutai_ble_bridge/run.sh
@@ -8,5 +8,12 @@ export MQTT_PASS=$(bashio::config 'mqtt_pass')
 # Pass devices list as JSON
 export JUTAI_DEVICES_JSON="$(bashio::config 'devices')"
 
+# Set DBUS system bus address
+export DBUS_SYSTEM_BUS_ADDRESS="unix:path=/var/run/dbus/system_bus_socket"
+
+# Activate virtual environment
+source /opt/venv/bin/activate
+
+# Run the bridge script
 echo "Starting JuTai BLE MQTT Bridge with devices from Add-on config..."
 python3 /usr/src/app/jutai_ble_mqtt_bridge.py
